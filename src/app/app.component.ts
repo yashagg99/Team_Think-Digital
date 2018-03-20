1535d86ba76a13de559e028b9449f60e30407663
shangkaul <shangkaul@gmail.com>
2018-03-20 07:53:42 +0530

    hi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,15 +8,21 @@ import { HomePage } from '../pages/home/home';
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
-    platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
-      splashScreen.hide();
+  rootPage: any;  // <<< Do not set the root page until the platform.ready()
+                  //      This avoids to face the plugin loading error.
+
+  pages: Array<{title: string, component: any}>;
+
+  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
+ 
+    this.platform.ready().then(() => {
+      this.rootPage = HomePage;    // <<< Set the first page after native side is ready.
+
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
     });
   }
+ 
 }
-
